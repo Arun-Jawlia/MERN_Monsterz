12832cc7ed3a408429741d7c9caf9975c3f2b10e
Arun-Jawlia <arunkumar08.mk@gmail.com>
2022-12-21 19:12:11 +0530

    a-1

--- Frontend/my-hackathon-app/pages/index.js
+++ Frontend/my-hackathon-app/pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,9 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>Home Page</h1>
-              </main>
+      <main>
+        <Navbar />
+      </main>
     </>
-  )
+  );
 }
